23b70da7a0ec119d640bcb5edddef5d1ca24b783
Mirtunjay Singh <msbok@ymail.com>
2020-08-22 18:26:37 +0530

    Added more methods

--- src/main/java/com/mirtunjay/morsereader/MorseReader.java
+++ src/main/java/com/mirtunjay/morsereader/MorseReader.java
@@ -1,12 +1,55 @@
 package com.mirtunjay.morsereader;
 
+import com.mirtunjay.morsereader.tree.MorseTree;
 import com.mirtunjay.morsereader.tree.Node;
 
 public class MorseReader {
-	Node morseTree;
+
+	private MorseTree morseTree;
 
 	public MorseReader() {
-		morseTree = new Node(); 
+		morseTree = new MorseTree();
 	}
 
+	public String translateMorse(String morseCode) {
+		// .... . .-.. .-.. --- / .-- --- .-. .-.. -..
+		if (morseCode == null) {
+			return null;
+		}
+		String[] tokens = morseCode.trim().split("\\s+");
+		StringBuilder sb = new StringBuilder();
+		for (String token : tokens) {
+			sb.append(getMorseCharacter(token));
+		}
+		return sb.toString();
+	}
+
+	private char getMorseCharacter(String token) {
+		if (token.equals("/")) {
+			return ' ';
+		} else if (token.replace(".", "").replace("-", "").length() != 0) {
+			return '$';
+		} else {
+			return traverse(token, morseTree.getRoot());
+		}
+
+	}
+
+	private char traverse(String token, Node root) {
+		Node currentNode = root;
+		for (int i = 0; i < token.length(); i++) {
+			if (token.charAt(i) == '.') {
+				if (currentNode.getLeftNode() == null) {
+					return '$';
+				}
+				currentNode = currentNode.getLeftNode();
+			} else {
+				if (currentNode.getRightNode() == null) {
+					return '$';
+				}
+				currentNode = currentNode.getRightNode();
+			}
+		}
+		return currentNode.getCharacter();
+	}
 }
--- src/main/java/com/mirtunjay/morsereader/tree/MorseTree.java
+++ src/main/java/com/mirtunjay/morsereader/tree/MorseTree.java
@@ -1,50 +1,56 @@
 package com.mirtunjay.morsereader.tree;
 
 public class MorseTree {
-	Node empty1 = new Node('_');
-	Node empty2 = new Node('_');
-	Node empty3 = new Node('_');
-	Node root = new Node('*');
-	Node a = new Node('a');
-	Node b = new Node('b');
-	Node c = new Node('c');
-	Node d = new Node('d');
-	Node e = new Node('e');
-	Node f = new Node('f');
-	Node g = new Node('g');
-	Node h = new Node('h');
-	Node i = new Node('i');
-	Node j = new Node('j');
-	Node k = new Node('k');
-	Node l = new Node('l');
-	Node m = new Node('m');
-	Node n = new Node('n');
-	Node o = new Node('o');
-	Node p = new Node('p');
-	Node q = new Node('q');
-	Node r = new Node('r');
-	Node s = new Node('s');
-	Node t = new Node('t');
-	Node u = new Node('u');
-	Node v = new Node('v');
-	Node w = new Node('w');
-	Node x = new Node('x');
-	Node y = new Node('y');
-	Node z = new Node('z');
-	Node n1 = new Node('1');
-	Node n2 = new Node('2');
-	Node n3 = new Node('3');
-	Node n4 = new Node('4');
-	Node n5 = new Node('5');
-	Node n6 = new Node('6');
-	Node n7 = new Node('7');
-	Node n8 = new Node('8');
-	Node n9 = new Node('9');
-	Node n0 = new Node('0');
-	Node dot = new Node('.');
-	Node comma = new Node(',');
-	Node questionMark = new Node('?');
-	
+	private Node u_right_empty = new Node('_');
+	private Node r_right_empty = new Node('_');
+	private Node o_left_empty = new Node('_');
+	private	Node o_right_empty = new Node('_');
+	private Node u_right_emp_left_empty= new Node('_');
+	private Node z_right_empty= new Node('_');
+	private	Node root = new Node('*');
+	private Node a = new Node('a');
+	private Node b = new Node('b');
+	private Node c = new Node('c');
+	private Node d = new Node('d');
+	private Node e = new Node('e');
+	private Node f = new Node('f');
+	private Node g = new Node('g');
+	private Node h = new Node('h');
+	private Node i = new Node('i');
+	private Node j = new Node('j');
+	private Node k = new Node('k');
+	private Node l = new Node('l');
+	private Node m = new Node('m');
+	private Node n = new Node('n');
+	private Node o = new Node('o');
+	private Node p = new Node('p');
+	private Node q = new Node('q');
+	private Node r = new Node('r');
+	private Node s = new Node('s');
+	private Node t = new Node('t');
+	private Node u = new Node('u');
+	private Node v = new Node('v');
+	private Node w = new Node('w');
+	private Node x = new Node('x');
+	private Node y = new Node('y');
+	private Node z = new Node('z');
+	private Node n1 = new Node('1');
+	private Node n2 = new Node('2');
+	private Node n3 = new Node('3');
+	private Node n4 = new Node('4');
+	private Node n5 = new Node('5');
+	private Node n6 = new Node('6');
+	private Node n7 = new Node('7');
+	private Node n8 = new Node('8');
+	private Node n9 = new Node('9');
+	private Node n0 = new Node('0');
+	private Node dot = new Node('.');
+	private Node comma = new Node(',');
+	private Node questionMark = new Node('?');
+	private Node plus = new Node('+');
+	private Node equalTo = new Node('=');
+	private Node forwardSlash = new Node('/');
+
 	public MorseTree() {
 		root.setLeftRight(e, t);
 		e.setLeftRight(i, a);
@@ -54,9 +60,30 @@ public class MorseTree {
 		n.setLeftRight(d, k);
 		m.setLeftRight(g, o);
 		s.setLeftRight(h, v);
-		u.setLeftRight(f, empty);
-		r.setLeftRight(l, empty);
+		u.setLeftRight(f, u_right_empty);
+		r.setLeftRight(l, r_right_empty);
+		w.setLeftRight(p, j);
+		d.setLeftRight(b, x);
+		k.setLeftRight(c, y);
+		g.setLeftRight(z, q);
+		o.setLeftRight(o_left_empty, o_right_empty);
+		h.setLeftRight(n5, n4);
+		v.setLeftRight(null, n3);
+		u_right_empty.setLeftRight(u_right_emp_left_empty, n2);
+		r_right_empty.setLeftRight(plus, null);
+		j.setLeftRight(null, n1);
+		b.setLeftRight(n6, equalTo);
+		x.setLeftRight(forwardSlash, null);
+		z.setLeftRight(n7, z_right_empty);
+		o_left_empty.setLeftRight(n8, null);
+		o_right_empty.setLeftRight(n9, n0);
+		u_right_emp_left_empty.setLeftRight(questionMark, null);
+		plus.setLeftRight(null, dot);
+		z_right_empty.setLeftRight(null, comma);
 		
-		
+	}
+
+	public Node getRoot() {
+		return root;
 	}
 }
